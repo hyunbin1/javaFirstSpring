079b04d7a2611416cb2d8ba9628fdf399d809cc5
hyunbin1 <kimgusqls1@gmail.com>
2021-06-23 00:05:32 +0900

    Spring first commit for practice

---
+++ src/main/java/Hello/hellospring/controller/HelloController.java
@@ -0,0 +1,55 @@
+package Hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+    @GetMapping("hello-mvc")
+        public String helloMvc(@RequestParam("name") String name, Model model){
+            model.addAttribute("name", name);
+            return "hello-template";
+
+        }
+
+        // API - html 없이 그냥 return 값만 보내준다.
+    @GetMapping("hello_string")
+    // html 바디 부분 직접 넣어준다.
+    @ResponseBody
+    public String helloBody(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+
+    // 진짜 사용할만한 데이터 API - json 형식
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+         // 꺼낼때
+        public String getName() {
+            return name;
+        }
+        // 데이터 넣을 때
+        public void setName(String name){
+            this.name = name;
+        }
+
+
+    }
+}
+
