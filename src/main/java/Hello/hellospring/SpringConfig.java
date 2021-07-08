69d233c536cf4206e8d4c0ffa12ec2807dba1ee5
hyunbin1 <kimgusqls1@gmail.com>
2021-07-08 12:30:01 +0900

    21/07/08 Create IntegrationTestCase

--- src/main/java/Hello/hellospring/SpringConfig.java
+++ src/main/java/Hello/hellospring/SpringConfig.java
@@ -1,13 +1,25 @@
 package Hello.hellospring;
 
 import Hello.hellospring.Service.MemberService;
+import Hello.hellospring.repository.JpaMemberRepository;
 import Hello.hellospring.repository.MemberRepository;
 import Hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+
 @Configuration
 public class SpringConfig {
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +28,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JpaMemberRepository(em);
     }
 }
