099db9ce37e99b18293a0baaea511c5843287949
hyunbin1 <kimgusqls1@gmail.com>
2021-07-08 12:14:59 +0900

    21/07/08 Using JPA with H2 Db

--- src/main/java/Hello/hellospring/domain/Member.java
+++ src/main/java/Hello/hellospring/domain/Member.java
@@ -1,8 +1,13 @@
 package Hello.hellospring.domain;
-// 도메인: 비지니스 도메인 객체, 즉 회원, 주문, 쿠폰 등등. 이는 주로 데이터베이스에 저장하고 관리됨.
-public class Member {
+import javax.persistence.*;
 
+// 도메인: 비지니스 도메인 객체, 즉 회원, 주문, 쿠폰 등등. 이는 주로 데이터베이스에 저장하고 관리됨.
+@Entity
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // db가 id 알아서 생성해준다.
     private Long id;
+
+//    @Column(name = "username")
     private String name;
 
     public Long getId() {
